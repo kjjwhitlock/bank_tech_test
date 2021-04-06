a1f6360ee6faf9706cd972e92a22ef4aa08af706
katiewhitlock <katiejwhitlock@gmail.com>
2021-04-06 17:11:40 +0100

    Rewrite first bank test

--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -2,14 +2,7 @@
 require 'bank.rb'
 
 describe Bank do
-  it "is a class" do
-    expect(Bank.new).to be_an_instance_of(Bank)
-  end
-end
-
-describe '#create' do
-  it "creates an instance of the 'Bank' class" do
-    @bank = Bank.create
-    expect(@bank).to be_an_instance_of(Bank)
+  it "it respond to #create" do
+    expect(Bank.new).to respond_to(:create)
   end
 end
