e534937ab66705163fc8f98621d0c3581ad0a0f2
katiewhitlock <katiejwhitlock@gmail.com>
2021-04-07 11:03:31 +0100

    Add withdraw and deposit methods: all tests passing

--- lib/bank.rb
+++ lib/bank.rb
@@ -5,4 +5,18 @@ class Bank
     @withdrawals = []
     @balance = 0
   end
+
+  def deposit(amount)
+    @deposits << amount
+    update_balance
+  end
+
+  def withdraw(amount)
+    @withdrawals << amount
+    update_balance
+  end
+  
+  def update_balance
+      @balance = (@deposits.sum - @withdrawals.sum)
+  end
 end
